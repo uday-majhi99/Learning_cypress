77b4c1f442b3b798120e4aeea0662344fc23aa4c
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2024-09-24 17:31:58 +0545

    new files added

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: '/Users/udaymajhi/Downloads/Cypress/cypress.config.js ',
+  },
+})
--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
-console.log("Hello WOlrd")
+// cypress/integration/google_spec.js
+describe('Open Google in Chrome', () => {
+    it('should open Google homepage', () => {
+        // Open Google's homepage
+        cy.visit('https://www.google.com');
+        
+        // Check if the Google logo is visible
+        cy.get('img[alt="Google"]').should('be.visible');
+    });
+});
